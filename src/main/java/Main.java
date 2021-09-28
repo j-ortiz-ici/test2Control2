71ea837566657f803ab4e299161933031c1dbc84
j-ortiz-ici <j.ortiz07@ufromail.cl>
2021-09-27 23:07:54 -0300

    Commit con menú y validaciones

---
+++ src/main/java/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static String getInput(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.next();
+    }
+    public static String getString(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        return scanner.nextLine();
+    }
+    //Ejecuta el programa
+    public static void startProgram(){
+        System.out.println("Bienvenido.");
+        while (true) {
+            showMenu();
+            int option = getMenuOption();
+            validateMenu(option);
+        }
+    }
+    //Muestra el menú
+    public static void showMenu() {
+        System.out.println("-------------");
+        System.out.println("[1] Mostrar trabajadores.");
+        System.out.println("[2] Registrar ingreso.");
+        System.out.println("[3] Mostrar licencias.");
+        System.out.println("[4] Registrar licencias médicas.");
+        System.out.println("[0] Salir (S/N)");
+    }
+    //Valida el input del usuario
+    public static int getMenuOption() {
+        while (true) {
+            String validacion = "01234";
+            String input = getInput("Seleccione una opción:");
+            if (validacion.indexOf(input) >= 0) {
+                int option = Integer.parseInt(input);
+                if (-1 < option && option < 5) {
+                    return option;
+                }
+            }
+            else {
+                System.out.println("Opción inválida");
+            }
+        }
+    }
+    //Switch con las opciones del menú
+    public static void validateMenu(int option) {
+        switch (option) {
+            case 1:
+                System.out.println("Mostrando trabajadores");
+                System.out.println("");
+                break;
+            case 2:
+
+                break;
+            case 3:
+                System.out.println("Mostrando licencias");
+                System.out.println("");
+                break;
+            case 4:
+
+                break;
+            case 0:
+                salir();
+                break;
+        }
+    }
+    public static void salir(){
+        String respuesta = getInput("Desea salir del programa? S/N");
+        respuesta = respuesta.toLowerCase();
+        if(respuesta.contains("s")){
+            System.exit(0);
+        }
+        else if (respuesta.contains("n")){
+            System.out.println("");
+            System.out.println("Hola soy yo");
+            System.out.println("Hola soy yo de nuevo");
+        }
+    }
+
+}
